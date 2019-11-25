1e64d965fbd7b8f528b4b3abebe085a976fb2385
platoneko <platonekosama@gmail.com>
2019-11-26 01:03:51 +0800

    [FIX] poj2503

---
+++ poj3714.cpp
@@ -0,0 +1,53 @@
+/*
+Raid
+
+Description
+After successive failures in the battles against the Union, the Empire
+retreated to its last stronghold. Depending on its powerful defense system,
+the Empire repelled the six waves of Union's attack. After several sleepless
+nights of thinking, Arthur, General of the Union, noticed that the only
+weakness of the defense system was its energy supply. The system was
+charged by N nuclear power stations and breaking down any of them would
+disable the system.
+The general soon started a raid to the stations by N special agents who were
+paradroped into the stronghold. Unfortunately they failed to land at the
+expected positions due to the attack by the Empire Air Force. As an
+experienced general, Arthur soon realized that he needed to rearrange the
+plan. The first thing he wants to know now is that which agent is the nearest
+to any power station. Could you, the chief officer, help the general to
+calculate the minimum distance between an agent and a station?
+Input
+The first line is a integer T representing the number of test cases.
+Each test case begins with an integer N (1 ≤ N ≤ 100000).
+The next N lines describe the positions of the stations. Each line consists of
+two integers X (0 ≤ X ≤ 1000000000) and Y (0 ≤ Y ≤ 1000000000) indicating
+the positions of the station.
+The next following N lines describe the positions of the agents. Each line
+consists of two integers X (0 ≤ X ≤ 1000000000) and Y (0 ≤ Y ≤ 1000000000)
+indicating the positions of the agent.
+Output
+For each test case output the minimum distance with precision of three
+decimal placed in a separate line.
+Sample Input
+2 4
+0 0
+0 1
+1 0
+1 1
+2 2
+2 3
+3 2
+3 3
+4
+0 0
+0 0
+0 0
+0 0
+0 0
+0 0
+0 0
+0 0
+Sample Output
+1.414
+0.000
+*/
