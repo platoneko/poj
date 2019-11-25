f467b84926d209e62d5a0a0683c476951f35a763
platoneko <platonekosama@gmail.com>
2019-11-26 00:28:29 +0800

    poj2503

---
+++ poj2503.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <map>
+#include <cstring>
+#include <string>
+using namespace std;
+
+int main() {
+    char buffer[30], en[11], forei[11];
+    map<string, string> dict;
+    while (fgets(buffer, sizeof(buffer), stdin)) {
+        if (strcmp(buffer, "\n") == 0) break;
+        sscanf(buffer, "%s %s", en, forei);
+        dict[string(forei)] = string(en);
+    }
+    while (scanf("%s", buffer) == 1) {
+        if (dict.count(buffer)) {
+            cout << dict[string(buffer)] << endl; 
+        } else {
+            cout << "eh" << endl; 
+        }
+    }
+    return 0;
+}
